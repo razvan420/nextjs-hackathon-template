5fa8e8eae89be7e16082ddeb72e2ec06b7f211ee
razvan <49594204+razvan420@users.noreply.github.com>
2025-06-02 05:09:32 +0300

    d

--- app/_components/Sponsors.tsx
+++ app/_components/Sponsors.tsx
@@ -5,21 +5,21 @@ import { SponsorType } from '../_template_data/Sponsor'
 export default function Sponsors({ data }: { data: SponsorType[] }) {
     return (
         <div id="sponsors" className='page component'>
-            <p className='title'>Sponsors</p>
-            <div className='row w-full'>
-                {data.map((obj, i) => (
-                    <div key={i} className='row justify-start m-10'>
-                        <Image
-                            src={obj.img}
-                            alt="sponsor img"
-                            width={200}
-                            height={160}
-                            className="w-[35vh] object-cover"
-                            placeholder="blur"
-                            blurDataURL={obj.img}
-                        />
-                    </div>
-                ))}
+            <p className='title'>AI Espionage</p>
+            <div className='row w-full justify-center'>
+                <div className='row justify-center m-10'>
+                    <Image
+                        src={data[0]?.img || '/default-sponsor.jpg'}
+                        alt="sponsor img"
+                        width={400}
+                        height={320}
+                        className="w-[35vh] object-cover"
+                        placeholder="empty"
+                        quality={100}
+                        priority={true}  // Load immediately since it's the only image
+                        sizes="35vh"
+                    />
+                </div>
             </div>
         </div>
     )
